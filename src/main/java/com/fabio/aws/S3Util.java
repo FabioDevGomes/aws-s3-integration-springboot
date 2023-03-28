680504510675ba5a3145ca861ce8a1dd0b1798bd
Fábio Dev <fabio.dev.gomes@gmail.com>
2023-03-28 17:43:52 -0300

    [CLOUD-v1.0] - S3 download showing in the browser

--- src/main/java/com/fabio/aws/S3Util.java
+++ src/main/java/com/fabio/aws/S3Util.java
@@ -1,21 +1,33 @@
 package com.fabio.aws;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.utils.IoUtils;
 
 public class S3Util {
 	public static final String BUCKET = "fabio-test-first-bucket";
+	static String keyFile = "";
+	static S3Client client;
 	
 	public static void uploadFile(String fileName, InputStream inputStream) throws S3Exception, AwsServiceException, SdkClientException, IOException{
 		setProperties();
-		S3Client client = S3Client.builder().build();
+		keyFile = fileName;
+		client = S3Client.builder().build();
 		
 		PutObjectRequest request = PutObjectRequest.builder()
 				.bucket(BUCKET)
@@ -31,6 +43,28 @@ public class S3Util {
 		System.setProperty("aws.region", "sa-east-1");
 		System.setProperty("aws.accessKeyId", "AKIAW3UJ24CPCXIY4YOR");
 		System.setProperty("aws.secretAccessKey", "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");
+	}
+	
+	public static byte[] downloadFile() throws IOException {
+		GetObjectRequest request = GetObjectRequest.builder()
+				.bucket(BUCKET)
+				.key(keyFile)
+				.build();
+		
+		ResponseInputStream<GetObjectResponse> response = client.getObject(request);
+
+		return IoUtils.toByteArray(response);
+		
+//		BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(keyFile));
+//		byte[] buffer = new byte[4096];
+//		int bytesRead = -1;
+//		
+//		while ((bytesRead = response.read(buffer)) != -1) {
+//			outputStream.write(buffer, 0, bytesRead);
+//		}
+		
+//		response.close();
+//		outputStream.close();
 	}
 	
 }
--- src/main/java/com/fabio/controller/FileS3Controller.java
+++ src/main/java/com/fabio/controller/FileS3Controller.java
@@ -1,10 +1,12 @@
 package com.fabio.controller;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,26 +20,23 @@ public class FileS3Controller {
 		return "awsUpload";
 	}
 	
-	@PostMapping("/fileAws/upload")
-	public String handlerUploadForm(Model model, String description, @RequestParam("file") MultipartFile file) {
+	@PostMapping(value = "/fileAws/upload", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+	public ResponseEntity<?> handlerUploadForm(String description, @RequestParam("file") MultipartFile file) {
 		String fileName = file.getOriginalFilename();
 		
-		System.out.println("Description "+ description);
-		System.out.println("File name "+ fileName);
+		System.out.println("::::: Description "+ description);
+		System.out.println("::::: File name "+ fileName);
 		
-		String message = "";
+		ByteArrayResource resource = null;
 		
 		try {
 			S3Util.uploadFile(fileName, file.getInputStream());
-			message = "Bro... You file has been uploaded successfully!";
+			resource = new ByteArrayResource(S3Util.downloadFile());
 		} catch (Exception e) {
-			message = "Erro uploading file: " + e.getMessage();
 			e.printStackTrace();
 		}
 		
-		model.addAttribute("message", message);
-		
-		return "message";
+		return ResponseEntity.status(HttpStatus.OK).contentLength(resource.contentLength()).body(resource);
 	}
 
 }
